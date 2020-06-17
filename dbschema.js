9ae826f1f2d889305ccfd246e12105f306eba6c7
ishitadatta <isheeta50@gmail.com>
2020-06-17 22:03:26 +0530

    First Commit

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            createdAt: 'June 17, 2020 at 1:00:00 AM UTC+5:30',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
---
+++ util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyAsjAy_luPNkOg51eHHLeVbhGoYG-0iObA",
+    authDomain: "socialape-76f27.firebaseapp.com",
+    databaseURL: "https://socialape-76f27.firebaseio.com",
+    projectId: "socialape-76f27",
+    storageBucket: "socialape-76f27.appspot.com",
+    messagingSenderId: "135896384128",
+    appId: "1:135896384128:web:e12323af7bef0ddd85dc65",
+    measurementId: "G-QKD1JKV92J"
+  };
